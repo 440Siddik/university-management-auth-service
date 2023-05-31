ebc680af550beecc910da2c8993ef9fbd5dcc56a
Afraaz <ahmedalif102@gmail.com>
2023-05-31 14:33:27 +0600

    [Done create Interface and Model]

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express, { Application, Request, Response } from 'express'
-import cors from "cors";
+import cors from 'cors'
 
-const app:Application = express();
+//Routes
+import userRoutes from './app/modules/users/user.route'
+
+const app: Application = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req:Request, res:Response) => {
-  res.send("Server is running");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is running')
+})
 
+app.use("/api/v1/" , userRoutes)
 
 export default app
---
+++ src/app/modules/users/user.route.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get('')
+router.post('')
+router.put('')
+router.patch('')
+router.delete('')
+
+export default router
